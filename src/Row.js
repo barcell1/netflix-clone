b39dd8bf5fcd6cf096af25670c8b7734c784dfe1
barcell1 <barcellg1@gmail.com>
2022-05-28 19:03:14 -0300

    first commit

---
+++ src/Row.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const baseurl = "https://image.tmdb.org/t/p/original/";
+
+// const scroll = (scrolloff) => {
+//   ref.current.scrollleft += scrolloff;
+// };
+function Row({ title, fetchUrl, isLargeRow }) {
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const [movies, setmovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setmovies(request.data.results);
+
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${baseurl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+        {/* <button onClick={() => scroll(20)} onclassName="row__button">
+          arrsdfdsgdsfgsdfgdsfgow
+        </button> */}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+
+export default Row;
